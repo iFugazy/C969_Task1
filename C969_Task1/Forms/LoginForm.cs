a245a52a5d3942b661b8f949e718d66cd76ba760
LabUser <LabUser@DESKTOP-BT4131D>
2024-08-03 03:52:13 +0000

    Got the connection to the MySQL server working.

--- C969_Task1/Forms/LoginForm.cs
+++ C969_Task1/Forms/LoginForm.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace C969_Task1
 {
@@ -20,6 +22,35 @@ namespace C969_Task1
             InitializeComponent();
             UserLocation.UserLocationString(this);
 
+            string connstring = null;
+            MySqlConnection cnn;
+
+            connstring = @"server=127.0.0.1; database=client_schedule; userid=sqlUser; password=Passw0rd!";
+            cnn = new MySqlConnection(connstring);
+
+            try
+            {
+                cnn.Open();
+                string query = "SELECT * FROM address;";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, cnn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "address");
+                DataTable dt = ds.Tables["address"];
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        MessageBox.Show(row[col] + "\t");
+                    }
+
+                    MessageBox.Show("\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No connection");
+            }
         }
 
 
