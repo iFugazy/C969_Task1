606146a19596e761d5047a17be426214c135f5ba
LabUser <LabUser@DESKTOP-BT4131D>
2024-08-06 20:31:01 +0000

    Got a selected string started for modifying data

--- C969_Task1/Forms/AddCustomerForm.cs
+++ C969_Task1/Forms/AddCustomerForm.cs
@@ -13,12 +13,15 @@ namespace C969_Task1.Forms
 {
     public partial class AddCustomerForm : Form
     {
+
         
 
         public AddCustomerForm()
         {
             InitializeComponent();
             
+
+
             
 
         }
@@ -27,13 +30,18 @@ namespace C969_Task1.Forms
         {
             MySqlConnection cnn = new MySqlConnection("datasource=127.0.0.1; port=3306; Username=sqlUser; Password=Passw0rd!");
             MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM client_schedule.customer", cnn);
+            DataSet ds = new DataSet();
 
             cnn.Open();
-
-            DataSet ds = new DataSet();
             adapter.Fill(ds, "customer");
             dataGridView1.DataSource = ds.Tables["customer"];
             cnn.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
         }
     }
 }
